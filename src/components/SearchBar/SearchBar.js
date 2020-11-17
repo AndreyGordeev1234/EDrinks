c6fdce149c0759208a9192212c3879ef992b198b
Andrey Gordeev <prolodgy1997@gmail.com>
2020-11-17 20:52:27 +0300

    Add search bar
    Added search bar as a
    component and set it in
    greeting component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,19 @@
+import './SearchBar.scss';
+import searchIcon from '../../assets/images/icons/search-icon.svg';
+
+const SearchBar = () => (
+  <form className="search-form">
+    <p className="search-form__info">
+      <input type="text" name="drink" 
+            className="search-form__field" 
+            placeholder="Search your drink"
+            autoComplete="off"/>
+      <button type="submit" 
+              className="search-form__submit">
+        <img src={searchIcon}/>
+      </button>
+    </p>
+  </form>
+)
+
+export default SearchBar;
