95ca26eaa699b32cdae4f744539dd07b0b2ea5a8
Andrey Gordeev <prolodgy1997@gmail.com>
2020-11-18 16:38:57 +0300

    Realize footer
    Realized footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,12 @@
 import './Footer.scss';
 
 const Footer = () => (
-  <>
-  </>
+  <footer className="footer">
+    Andrey Gordeev |
+    <a className="footer__link" href="https://github.com/AndreyGordeev1234">
+      GitHub
+    </a>
+  </footer>
 )
 
 export default Footer;
