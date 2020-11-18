940657efdd1dd88df3d24bd1c337ec0b086f2717
Andrey Gordeev <prolodgy1997@gmail.com>
2020-11-18 16:02:56 +0300

    Realize products component
    Realize products and product
    component without real data

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,33 @@
 import './Product.scss';
+import productImage from '../../assets/images/product.png';
+import addToCardIco from '../../assets/images/icons/add-to-card-icon.svg'
 
 const Product = () => (
-  <>
-  </>
+  <div className="products__card">
+    <div className="products__card-pic-wrap">
+      <img src={productImage}
+        alt="Stranberry and blue"
+        className="products__card-pic"/>
+    </div>
+    <div className="products__card-info-wrapper">
+      <h3 className="products__card-title">
+        Stranberry and blue
+      </h3>
+      <p className="products__card-desc">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi mauris.
+      </p>
+      <div className="products__card-footer">
+        <p className="products__card-price">
+          $15.00
+        </p>
+        <button className="products__card-buy">
+          <img src={addToCardIco}
+            alt="Add to cart"
+            className="products__card-cart-icon"/>
+        </button>
+      </div>
+    </div>
+  </div>
 )
 
 export default Product;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,8 +1,24 @@
 import './Products.scss';
+import Product from '../Product/Product';
 
 const Products = () => (
-  <>
-  </>
+  <section className="products">
+    <div className="products__wrapper">
+      <h2 className="products__title">All</h2>
+      <div className="products__cards">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </div>
+  </section>
 )
 
 export default Products;
