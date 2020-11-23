40f4941da47d54bc2ba29a03c21f2737d56e9ab5
Andrey Gordeev <prolodgy1997@gmail.com>
2020-11-23 13:41:50 +0300

    Add spinner
    Added spinner component and
    make it to render on products
    loading

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,17 +1,23 @@
 import './Products.scss';
 import Product from '../Product/Product';
+import Spinner from '../Spinner';
 
-const Products = ({ products }) => (
-  <section className="products">
-    <div className="products__wrapper">
-      <h2 className="products__title">All</h2>
-      <div className="products__cards">
-        {products.map(product => (
-          <Product key={product.id} {...product}/>
-        ))}
+const Products = ({ products, isLoading }) => {
+  if (isLoading) return <Spinner />
+
+  return (
+    <section className="products">
+      <div className="products__wrapper">
+        <h2 className="products__title">All</h2>
+        <div className="products__cards">
+          {products.map(product => (
+            <Product key={product.id} {...product}/>
+          ))}
+        </div>
       </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
+
 
 export default Products;
